bf4de97205976d09fd7b4c1a7a7461d79561328c
AlexC01 <acuadraq@gmail.com>
2025-08-14 20:54:26 -0400

    ui: fixing quick ui style from input

--- src/app/_components/Inputs/NumberInput.tsx
+++ src/app/_components/Inputs/NumberInput.tsx
@@ -29,7 +29,7 @@ const NumberInput = ({
 				id={id}
 				value={value}
 				onChange={(e) => onChange(e.target.value)}
-				className="rounded border-1 border-gray-300 shadow-sm pl-3 py-2 w-full"
+				className="rounded border-1 border-gray-300 shadow-sm pl-3 pr-2 py-2 w-full"
 			/>
 		</div>
 	);
