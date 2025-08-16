4ef096f60d37d26f56ca623d9da9bd308ce52cdf
AlexC01 <acuadraq@gmail.com>
2025-08-16 14:01:59 -0400

    feat: adding loading skeletong for the product detail

--- src/app/products/[slug]/loading.tsx
+++ src/app/products/[slug]/loading.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
-
 const loading = () => {
-	return <div>loading</div>;
+	return (
+		<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-10 pt-14 pb-16">
+			<section className="flex flex-col md:flex-row  justify-between gap-12">
+				<div className="w-1/2 h-80 rounded-md bg-gray-400 mb-5" />
+
+				<div className="md:w-1/2 text-center md:text-left animate-pulse">
+					<div className="size-3 w-full rounded-full bg-gray-400 mb-5" />
+					<div>
+						<div className="size-3 w-full rounded-full bg-gray-400 mb-5" />
+					</div>
+					<hr className="mt-2 text-gray-400" />
+					<div className="size-3 w-full rounded-full bg-gray-400 mb-5" />
+					<div className="mt-2">
+						<div className="size-3 w-full rounded-full bg-gray-400 mb-5" />
+					</div>
+					<div className="size-3 w-full rounded-full bg-gray-400 mb-5" />
+					<div className="size-3 w-full rounded-full bg-gray-400 mb-5" />
+					<div className="size-3 w-full rounded-full bg-gray-400 mb-5" />
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default loading;
