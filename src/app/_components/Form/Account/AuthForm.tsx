041ea30ed21ab1e4b79329374975e61b389ae7ae
AlexC01 <acuadraq@gmail.com>
2025-08-20 22:34:38 -0400

    feat: adding log in button in the form

--- src/app/_components/Form/Account/AuthForm.tsx
+++ src/app/_components/Form/Account/AuthForm.tsx
@@ -34,6 +34,24 @@ const AuthForm = () => {
 					</div>
 				</form>
 			</div>
+			<div className="mt-5 flex justify-end">
+				<span className="inline-block text-gray-500 font-medium underline cursor-pointer text-sm hover:text-gray-800 transition-colors duration-200">
+					Forgot Password?
+				</span>
+			</div>
+
+			<button
+				type="button"
+				className="bg-accent-500 text-white font-bold mt-6 cursor-pointer rounded-md uppercase py-3 px-4 w-full transition-all duration-200 shadow-xl hover:shadow-sm"
+			>
+				Log In
+			</button>
+
+			<div className="mt-8 flex items-center justify-center gap-4">
+				<div className="h-[1px] w-full bg-gray-400" />
+				<p>OR</p>
+				<div className="h-[1px] w-full bg-gray-400" />
+			</div>
 		</>
 	);
 };
