f4f281a1a183ec10760e29a7dee408b3d9f87449
AlexC01 <acuadraq@gmail.com>
2025-08-21 20:10:00 -0400

    feat: adding the base toast at the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Toaster } from 'react-hot-toast';
 import Footer from './_components/Footer/Footer';
 import Navbar from './_components/Navbar/Navbar';
 
@@ -29,6 +30,10 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				<Toaster
+					position="top-center"
+					toastOptions={{ style: { marginTop: '60px' } }}
+				/>
 				<Navbar />
 				<main className="min-h-screen ">{children}</main>
 				<Footer />
