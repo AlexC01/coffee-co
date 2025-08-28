5f39787295feb303997a0c0c28f9c45b6c6a1fde
AlexC01 <acuadraq@gmail.com>
2025-08-27 20:41:30 -0400

    feat: adding cart store and model description: adding zustand store to keep the state of the cart through all of the app and syncronized with firebase

---
+++ src/app/lib/models/Cart.ts
@@ -0,0 +1,14 @@
+export interface CartItem {
+	color: string;
+	image: string;
+	name: string;
+	price: number;
+	quantity: number;
+	size: string;
+}
+
+export type CartItems = Record<string, CartItem>;
+
+export interface CartInterface {
+	items: CartItems;
+}
---
+++ src/app/lib/store/useCartStore.ts
@@ -0,0 +1,28 @@
+import { doc, onSnapshot } from 'firebase/firestore';
+import { create } from 'zustand';
+import { db } from '../firebase';
+import type { CartItems } from '../models/Cart';
+
+interface CartState {
+	items: CartItems;
+	setCart: (items: CartItems) => void;
+	subscribeToCart: (userId: string) => void;
+}
+
+export const useCartStore = create<CartState>()((set) => ({
+	items: {},
+	setCart: (items) => set({ items }),
+
+	subscribeToCart: (userId) => {
+		const cartRef = doc(db, 'carts', userId);
+
+		const unsubscribe = onSnapshot(cartRef, (docSnap) => {
+			if (docSnap.exists()) {
+				const cart = docSnap.data() as { items: CartItems };
+				set({ items: cart.items });
+			} else set({ items: {} });
+		});
+
+		return unsubscribe;
+	},
+}));
