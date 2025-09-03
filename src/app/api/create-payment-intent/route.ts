6b21e668ce3e4fca815fe0932f50a0ef359b9d1d
AlexC01 <acuadraq@gmail.com>
2025-09-03 12:33:32 -0400

    refactor: adding the refactor to the POST api

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -5,12 +5,18 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export const POST = async (req: Request) => {
 	try {
-		const { amount } = await req.json();
+		const { amount, cartItem } = await req.json();
+
+		const cartId = JSON.stringify(cartItem);
 
 		const paymentIntent = await stripe.paymentIntents.create({
 			amount: amount * 100,
 			currency: 'usd',
 			automatic_payment_methods: { enabled: true },
+			description: 'Order from Coffee-Co',
+			metadata: {
+				cart: cartId,
+			},
 		});
 
 		return NextResponse.json({ clientSecret: paymentIntent.client_secret });
