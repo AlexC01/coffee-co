19e535396409190c41e813f5e8afd2c49cd95fb6
AlexC01 <acuadraq@gmail.com>
2025-09-03 21:26:22 -0400

    feat: adding more not found pages and images

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,25 @@
+import Image from 'next/image';
+import notFoundImage from '@/../public/images/notFound.svg';
+import BreadCrumb from './_components/BreadCrumb';
+import { routes } from './lib/models/Routes';
+
 const notFound = () => {
-	return <div>notFound</div>;
+	return (
+		<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-10 pt-28">
+			<BreadCrumb links={[{ label: 'Home', route: routes.home }]} />
+			<div className="mt-10 flex items-center justify-center flex-col">
+				<Image
+					src={notFoundImage}
+					width={350}
+					height={350}
+					alt="Not Found Image"
+				/>
+				<p className="text-2xl font-bold mt-15">
+					It looks like this page does not exist in our website
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default notFound;
--- src/app/products/[slug]/not-found.tsx
+++ src/app/products/[slug]/not-found.tsx
@@ -1,5 +1,30 @@
+import Image from 'next/image';
+import notFoundImage from '@/../public/images/notFoundSingleProd.svg';
+import BreadCrumb from '@/app/_components/BreadCrumb';
+import { routes } from '@/app/lib/models/Routes';
+
 const notFound = () => {
-	return <div>not-found</div>;
+	return (
+		<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-10 pt-28">
+			<BreadCrumb
+				links={[
+					{ label: 'Home', route: routes.home },
+					{ label: 'Products', route: routes.products },
+				]}
+			/>
+			<div className="mt-10 flex items-center justify-center flex-col">
+				<Image
+					src={notFoundImage}
+					width={350}
+					height={350}
+					alt="Not Found Image"
+				/>
+				<p className="text-2xl font-bold mt-15">
+					It looks like this product does not exist in our database
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default notFound;
